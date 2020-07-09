a051347da2d846d95b6caef6c75024d9a75d7df0
Ovy95 <jackoverton21@hotmail.com>
2020-07-09 11:01:46 +0100

    Upperlowercase task complete

--- Channels/7 Channel_assignementUpperLowercase.go
+++ Channels/7 Channel_assignementUpperLowercase.go
@@ -3,19 +3,35 @@
 
 package main
 
+import (
+	"fmt"
+	"strings"
+	"time"
+)
+
 func main() {
 	text := "Please Modify This Text!"
-
 	cUpper := make(chan string)
 	clower := make(chan string)
 
+	fmt.Printf("\ntext before uppercase \n  %s\n \n", text)
 	go uppercase(text, cUpper)
+	go lowercase(text, clower)
+	sUpper, sLower := <-cUpper, <-clower
+	fmt.Printf("sUpper=%s \n", sUpper)
+
+	fmt.Printf("slower=%s \n", sLower)
+
 }
 
 func uppercase(s string, c chan string) {
-	c <- string.ToUpper(s)
+	fmt.Println("(f) entering uppercase()")
+	time.Sleep(3 * time.Second)
+	c <- strings.ToUpper(s)
 }
 
 func lowercase(s string, c chan string) {
-	c <- string.ToLower(s)
+	fmt.Println("(f) entering lowercase()")
+	time.Sleep(3 * time.Second)
+	c <- strings.ToLower(s)
 }
